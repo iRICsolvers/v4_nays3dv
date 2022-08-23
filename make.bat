c409fba28758e98a3941c0dcd183eb03ff5e7ef3
starfieldsKt <keita.hoshino.1001@gmail.com>
2022-08-23 16:43:13 +0900

    make.batの中身を更新

--- make.bat
+++ make.bat
@@ -6,8 +6,22 @@ rem ----------------------------------------------------------------------
 rem ifort compile
 rem ----------------------------------------------------------------------
 ifort .\src\iric.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
-ifort  /F100000000 /f77rtl /fixed /extend-source:132 /MD /c .\src\nays2d+.f
-ifort *.obj .\lib\iriclib.lib -o ".\install\nays2d+.exe"
+ifort .\src\common.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\output.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\ss_nu.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\cell2node.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\cip3d.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\diffusion.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\initial.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\newgrd.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\non_advection.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\o3upwind.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\omgpsi.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\upwind3d.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\w12cal.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\bound.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort .\src\nays3dv.f90  /Qopenmp /nostandard-realloc-lhs /MD /c
+ifort *.obj .\lib\iriclib.lib -o ".\install\nays3dv.exe"
 
 del *.obj
 del *.mod
